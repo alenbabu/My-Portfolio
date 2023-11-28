e5877f001f32fbe1d4c25f510dc1b800ab5115ff
Alen Babu <alenbabualen@gmail.com>
2023-11-29 00:07:59 +0530

    github actions updated

--- src/Components/Firstpage/Firstpage.jsx
+++ src/Components/Firstpage/Firstpage.jsx
@@ -9,7 +9,7 @@ function Firstpage() {
                         <div className="absolute -inset-0.5 bg-gradient-to-r from-cyan-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
                         <button className="relative px-10 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
                             <span className="flex items-center space-x-5">
-                                <span className="bg-gradient-to-r from-purple-600 to-cyan-600 bg-clip-text text-transparent font-bold text-xl">ALEN BABUu</span>
+                                <span className="bg-gradient-to-r from-purple-600 to-cyan-600 bg-clip-text text-transparent font-bold text-xl">ALEN BABU</span>
                             </span>
                         </button>
                     </div>
